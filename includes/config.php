0251ba0d485945788b4e0d2f5d876938ad42f8be
chris <chris@chris-ThinkPad-T60.(none)>
2013-03-16 10:18:40 +0100

    add config file : list rss feed

---
+++ includes/config.php
@@ -0,0 +1,8 @@
+<?PHP
+$siteList = array(
+   'pcinpact'        => "http://www.pcinpact.com/include/news.xml",
+   'nettuts'         =>   "http://feeds.feedburner.com/nettuts",
+   'tt-hardware'     =>   "http://www.tt-hardware.com/rss",
+   'android-france'  =>   "http://feeds.feedburner.com/android-france",
+);
+?>
